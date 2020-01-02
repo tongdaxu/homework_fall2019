e59b6fc725a16674a7b16a03b8a3b5a0303ddb87
tongdaxu <matonglidewazi@qq.com>
2020-01-02 09:17:58 -0500

    hw 2 policy gradient

--- hw2/cs285/infrastructure/rl_trainer.py
+++ hw2/cs285/infrastructure/rl_trainer.py
@@ -141,12 +141,36 @@ class RL_Trainer(object):
 
     def collect_training_trajectories(self, itr, load_initial_expertdata, collect_policy, batch_size):
         # TODO: GETTHIS from HW1
+        if itr==0:
+            with open(load_initial_expertdata, 'rb') as f:
+                loaded_paths=pickle.loads(f.read())
+            return loaded_paths, 0, None
+
+        print("\nCollecting data to be used for training...")
+        paths, envsteps_this_batch = sample_trajectories(self.env, collect_policy, batch_size, self.params['ep_len'])
+
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
+        # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
+        train_video_paths = None
+        if self.log_video:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            train_video_paths = sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
+
+        return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
         # TODO: GETTHIS from HW1
+        print('\nTraining agent using sampled data from replay buffer...')
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(self.params['train_batch_size'])
+            self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
 
     def do_relabel_with_expert(self, expert_policy, paths):
         # TODO: GETTHIS from HW1 (although you don't actually need it for this homework)
+        print("\nRelabelling collected observations with labels from an expert policy...")
+        for path in paths:
+            path["action"]=expert_policy.get_action(path["observation"])
+        return paths
 
     ####################################
     ####################################
